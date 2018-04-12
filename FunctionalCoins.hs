5f512c0f74314c176d1f15af1147c4b55ab0bc8b
Victor <victork95@hotmail.com.ar>
2018-04-12 17:08:55 -0300

    (8-10) tests

--- FunctionalCoins.hs
+++ FunctionalCoins.hs
@@ -32,7 +32,7 @@ cierreDeCuenta usuario = usuario{ billetera = 0}
 quedaIgual usuario = usuario
 
 ejecutarTests = hspec $ do
- describe "Cosas que pasan" $ do
+ describe "Eventos: Cosas que pasan con una billetera de 10 monedas" $ do
   it "Depósito 10 monedas, ahora tiene 20 monedas" $ (billetera (depósito rodrigo 10)) `shouldBe` 20
   it "Extrajo 3 monedas, ahora tiene 7 monedas" $ (billetera (extracción rodrigo 3)) `shouldBe` 7
   it "Extrajo 15 monedas, ahora tiene 0 monedas" $ (billetera (extracción rodrigo 15)) `shouldBe` 0
@@ -40,3 +40,7 @@ ejecutarTests = hspec $ do
   it "Cuenta cerrada, no quedaron ni mariposas" $ (billetera (cierreDeCuenta rodrigo)) `shouldBe` 0
   it "No hubo cambios, sigue con 10 monedas" $ (billetera (quedaIgual rodrigo)) `shouldBe` 10
   it "Depósito 1000 monedas y tuvo un upgrade, ahora tiene 1020" $ ((billetera.upgrade.depósito rodrigo) 1000) `shouldBe` 1020
+ describe "Usuarios: Casos con Pepe y Lucho" $ do 
+  it "Pepe tiene 10 monedas" $ (billetera pepe) `shouldBe` 10
+  it "A pepe le cierran la cuenta entonces queda con 0 monedas" $ (billetera (cierreDeCuenta pepe)) `shouldBe` 0
+  it "A pepe le depositan 15 monedas, extrae 2 y recibe un upgrade, y le termina quedando 27.6 monedas" $ ((billetera.upgrade.extracción (depósito pepe 15)) 2) `shouldBe` 27.6
