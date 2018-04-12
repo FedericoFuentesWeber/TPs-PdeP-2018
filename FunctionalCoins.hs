c58657efb5db4ae56f1ae7c1780b6b97f20e413a
Victor <victork95@hotmail.com.ar>
2018-04-12 16:37:47 -0300

    Def data (smallchange) + upgrade + cierre + igual + (1-7)tests

--- FunctionalCoins.hs
+++ FunctionalCoins.hs
@@ -2,14 +2,17 @@
 import Text.Show.Functions
 import Data.List
 import Data.Maybe
---import Test.Hspec
+import Test.Hspec
 
 data Usuario = Usuario{
-  billetera :: Int,
+  nombre :: String,
+  billetera :: Float,
   nivel :: Int
 } deriving(Show)
 
-rodrigo = Usuario 10 0
+rodrigo = Usuario "Rodrigo" 10 0
+pepe = Usuario "José" 10 0
+lucho = Usuario "Luciano" 2 0
 
 sumarDinero usuario dinero = (billetera usuario) + dinero
 depósito usuario dineroDepositado = usuario{ billetera = sumarDinero usuario dineroDepositado}
@@ -18,8 +21,22 @@ restarDinero usuario dinero = (billetera usuario) - dinero
 extracción usuario dineroExtraido
  |restarDinero usuario dineroExtraido > 0 = usuario{ billetera = restarDinero usuario dineroExtraido}
  |otherwise = usuario{ billetera = 0}
+ 
+subirUnNivel usuario = (nivel usuario) + 1
+upgrade usuario
+ |billetera usuario <= 50 = usuario{ billetera = (billetera usuario) * 1.20, nivel = subirUnNivel usuario}
+ |billetera usuario > 50 = usuario{ billetera = sumarDinero usuario 10, nivel = subirUnNivel usuario}
 
---upgrade usuario
+cierreDeCuenta usuario = usuario{ billetera = 0}
 
---cierreDeCuenta usuario =
---quedaIgual usuario =
+quedaIgual usuario = usuario
+
+ejecutarTests = hspec $ do
+ describe "Cosas que pasan" $ do
+  it "Depósito 10 monedas, ahora tiene 20 monedas" $ (billetera (depósito rodrigo 10)) `shouldBe` 20
+  it "Extrajo 3 monedas, ahora tiene 7 monedas" $ (billetera (extracción rodrigo 3)) `shouldBe` 7
+  it "Extrajo 15 monedas, ahora tiene 0 monedas" $ (billetera (extracción rodrigo 15)) `shouldBe` 0
+  it "Tuvo un upgrade, ahora tiene 12 monedas" $ (billetera (upgrade rodrigo)) `shouldBe` 12
+  it "Cuenta cerrada, no quedaron ni mariposas" $ (billetera (cierreDeCuenta rodrigo)) `shouldBe` 0
+  it "No hubo cambios, sigue con 10 monedas" $ (billetera (quedaIgual rodrigo)) `shouldBe` 10
+  it "Depósito 1000 monedas y tuvo un upgrade, ahora tiene 1020" $ ((billetera.upgrade.depósito rodrigo) 1000) `shouldBe` 1020
